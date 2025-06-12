5cd744f8287fad3420f0eb739cd77897cf47cc01
Zdvictor <victor.zaidir@gmail.com>
2025-06-12 17:09:31 -0300

    API SHIRT STORE V2

--- knexfile.js
+++ knexfile.js
@@ -31,6 +31,37 @@ module.exports = {
           }
 
     },
+
+    production: {
+
+        client: "mysql2",
+
+        connection: {
+
+            host: process.env.DB_HOST,
+            user: process.env.DB_USER,
+            password: process.env.DB_PASSWORD,
+            database: process.env.DB_DATABASE,
+            timezone: "-03:00"
+        
+
+        },
+
+        pool: {
+
+            min: 2,
+            max: 10
+
+        },
+
+        migrations: {
+            directory: './src/database/migrations'
+          },
+          seeds: {
+            directory: './src/database/seeds'
+          }
+
+    },
     
 
 }
