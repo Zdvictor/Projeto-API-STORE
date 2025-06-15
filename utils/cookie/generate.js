b7cf82e900db94bd9a10263c7814715cb5882743
Zdvictor <victor.zaidir@gmail.com>
2025-06-15 11:03:59 -0300

    API SHIRT STORE V2

--- utils/cookie/generate.js
+++ utils/cookie/generate.js
@@ -1,6 +1,10 @@
+// Arquivo: utils/cookie/generate.js
+module.exports = function (res, token) {
 
-module.exports = function (res,token) {
-
-    res.cookie("token", token, { httpOnly: true, secure: false, sameSite: "strict" })   
+    res.cookie("token", token, { 
+        httpOnly: true, 
+        secure: true,
+        sameSite: "none"
+    });
 
 }
